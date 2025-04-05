53f7c149f47e0dea075f3375da780d892ddb2bc4
Beuss <beuss.code@gmail.com>
2025-04-05 19:53:59 +0200

    Initial source

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# ubx-nav-pvt-foxglove-converter version history
+
+## 0.0.0
+
+- Alpha testing
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { ExtensionContext, MessageEvent } from "@foxglove/extension";
+
+type GNSSNavPvt = {
+  lat: number;
+  lon: number;
+};
+
+export function activate(extensionContext: ExtensionContext) {
+    extensionContext.registerMessageConverter({
+    fromSchemaName: "blueboat_interfaces/msg/GNSSNavPvt",
+    toSchemaName: "foxglove.LocationFix",
+    converter: (ubxNavPvt: GNSSNavPvt, _: MessageEvent<GNSSNavPvt>) => {
+      return {
+        latitude: ubxNavPvt.lat,
+        longitude: ubxNavPvt.lon,
+      };
+    },
+  });
+}
